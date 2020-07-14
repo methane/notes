1618235f7731b9e78cd1b854b84cc584825ec673
Inada Naoki <songofacandy@gmail.com>
2020-07-14 17:47:21 +0900

    zap: example

---
+++ zap/go.mod
@@ -0,0 +1,5 @@
+module zap-sb
+
+go 1.14
+
+require go.uber.org/zap v1.15.0 // indirect
---
+++ zap/sample.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	sugar := zap.NewExample().Sugar()
+	defer sugar.Sync()
+	sugar.Infow("failed to fetch URL",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("failed to fetch URL: %s", "http://example.com")
+}
