befa7c33bbbb512efc991eb4796e2c26acd7e06c
Inada Naoki <songofacandy@gmail.com>
2024-02-27 18:50:13 +0900

    試した結果をREADMEにまとめた

--- 2024/otel-proto/README.md
+++ 2024/otel-proto/README.md
@@ -1,3 +1,11 @@
 # otel-proto
 
-Describe your project here.
+PythonでOpenTelemetryのプロトコルを触ってみる。
+protobuf や protobuf json へのエンコード、デコードはできるが、
+そこから ReadableSpan 等にするには otel のライブラリが用意している
+encode系の関数の逆変換をする関数を自前で実装しないといけないことがわかった。
+
+エンコード速度は、pb2がjsonの2倍以上速く、エンコード後のサイズは1/3以下になった。
+
+トレースには同じ情報が大量に含まれるので、Batch化しておくと圧縮がかなり効く。
+圧縮後のサイズはjsonもpb2も大きく変わらない。
--- 2024/otel-proto/file_export.py
+++ 2024/otel-proto/file_export.py
@@ -39,7 +39,7 @@ def do_work():
         pass
         # When the 'with' block goes out of scope, 'span' is closed for you
 
-N = 10  # sizeとtimingを測るときは100を使った
+N = 100  # sizeとtimingを測るときは100を使った
 for _ in range(N):
     do_work()
 
@@ -58,25 +58,12 @@ from opentelemetry.exporter.otlp.proto.common._internal.trace_encoder import (
 )
 from google.protobuf import json_format
 
-## spanをjsonにしてpb messageに戻すまで
-# FileExporter format は json lines (jsonl) なので、改行を入れないで
-# エンコードする必要がある.
-# from opentelemetry.proto.trace.v1.trace_pb2 import Span as PB2SPan
-
-# print(f"{spans[0]=}")
-# encoded_span = _encode_span(spans[0])
-# print(f"{encoded_span=}")
-# json_span = json_format.MessageToJson(encoded_span, indent=None)
-# print(f"{json_span=}")
-# json_spans2 = json_format.Parse(json_span, PB2SPan())
-# print(f"{json_spans2=}")
-
 ## spanをpb2, json化
 encoded_spans = encode_spans(spans)
 # File Exporrt format は json lines なのでindent=Noneで1行にする
 json_spans = json_format.MessageToJson(encoded_spans, indent=None)
-print("json output:", json_spans)
-
+print("json output:")
+print(json_spans)
 
 ## compare timing
 # import timeit
@@ -86,16 +73,21 @@ print("json output:", json_spans)
 ## timing_json=1.6855301250470802, timing_pb2=0.6994002499850467
 
 ## compare size
-# pb2_spans = encode_spans(spans).SerializeToString()
-# print(f"{len(json_spans)=} vs {len(pb2_spans)=}")
-## len(json_spans)=22202 vs len(pb2_spans)=6472
+json_spans = json_spans.encode()
+pb2_spans = encode_spans(spans).SerializeToString()
+print(f"{len(json_spans)=} vs {len(pb2_spans)=}")
+# len(json_spans)=22202 vs len(pb2_spans)=6472
+
+import gzip
+print(f"{len(gzip.compress(json_spans))=} vs {len(gzip.compress(pb2_spans))=}")
+# len(gzip.compress(json_spans))=3972 vs len(gzip.compress(pb2_spans))=3686
 
 ## FileExporter formatを取り込んで、別のExporterから出力してみる。
 
-from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import (
-    ExportTraceServiceRequest as PB2ExportTraceServiceRequest,
-)
-request = json_format.Parse(json_spans, PB2ExportTraceServiceRequest())
-from google.protobuf import text_format
-print("protobuf(text):")
-text_format.PrintMessage(request, sys.stdout))
+# from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import (
+#     ExportTraceServiceRequest as PB2ExportTraceServiceRequest,
+# )
+# request = json_format.Parse(json_spans, PB2ExportTraceServiceRequest())
+# from google.protobuf import text_format
+# print("protobuf(text):")
+# text_format.PrintMessage(request, sys.stdout)
