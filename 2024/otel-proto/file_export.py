9c6407d502be2b1b00e8aeace360dcdb468a128d
Inada Naoki <songofacandy@gmail.com>
2024-02-27 18:42:49 +0900

    protobuf jsonからデコードまで試した

--- 2024/otel-proto/file_export.py
+++ 2024/otel-proto/file_export.py
@@ -3,6 +3,8 @@
 # まずはメモリ上にトレースを生成する。
 # https://opentelemetry.io/docs/languages/python/instrumentation/#acquire-tracer
 
+import sys
+
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
@@ -20,9 +22,9 @@ exporter = InMemorySpanExporter()
 processor = BatchSpanProcessor(exporter)
 provider.add_span_processor(processor)
 # ConsoleSpanExporterは1spanずつprintするので、バッチ化するメリットがない.
-provider.add_span_processor(
-    SimpleSpanProcessor(ConsoleSpanExporter())
-)
+# provider.add_span_processor(
+#     SimpleSpanProcessor(ConsoleSpanExporter())
+# )
 
 # Sets the global default tracer provider
 trace.set_tracer_provider(provider)
@@ -33,11 +35,12 @@ tracer = trace.get_tracer("my.tracer.name")
 def do_work():
     with tracer.start_as_current_span("span-name") as span:
         # do some work that 'span' will track
-        print("doing some work...")
+        # print("doing some work...")
+        pass
         # When the 'with' block goes out of scope, 'span' is closed for you
 
-
-for _ in range(10):
+N = 10  # sizeとtimingを測るときは100を使った
+for _ in range(N):
     do_work()
 
 processor.force_flush()
@@ -50,9 +53,49 @@ spans = exporter.get_finished_spans()
 from opentelemetry.exporter.otlp.proto.common.trace_encoder import (
     encode_spans,
 )
-from google.protobuf.json_format import MessageToJson
+from opentelemetry.exporter.otlp.proto.common._internal.trace_encoder import (
+    _encode_span
+)
+from google.protobuf import json_format
 
+## spanをjsonにしてpb messageに戻すまで
 # FileExporter format は json lines (jsonl) なので、改行を入れないで
 # エンコードする必要がある.
-json_spans = MessageToJson(encode_spans(spans), indent=None)
-print(json_spans)
+# from opentelemetry.proto.trace.v1.trace_pb2 import Span as PB2SPan
+
+# print(f"{spans[0]=}")
+# encoded_span = _encode_span(spans[0])
+# print(f"{encoded_span=}")
+# json_span = json_format.MessageToJson(encoded_span, indent=None)
+# print(f"{json_span=}")
+# json_spans2 = json_format.Parse(json_span, PB2SPan())
+# print(f"{json_spans2=}")
+
+## spanをpb2, json化
+encoded_spans = encode_spans(spans)
+# File Exporrt format は json lines なのでindent=Noneで1行にする
+json_spans = json_format.MessageToJson(encoded_spans, indent=None)
+print("json output:", json_spans)
+
+
+## compare timing
+# import timeit
+# timing_json = timeit.timeit(lambda: json_format.MessageToJson(encode_spans(spans)), number=1000)
+# timing_pb2  = timeit.timeit(lambda: encode_spans(spans).SerializeToString(), number=1000)
+# print(f"{timing_json=}, {timing_pb2=}")
+## timing_json=1.6855301250470802, timing_pb2=0.6994002499850467
+
+## compare size
+# pb2_spans = encode_spans(spans).SerializeToString()
+# print(f"{len(json_spans)=} vs {len(pb2_spans)=}")
+## len(json_spans)=22202 vs len(pb2_spans)=6472
+
+## FileExporter formatを取り込んで、別のExporterから出力してみる。
+
+from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import (
+    ExportTraceServiceRequest as PB2ExportTraceServiceRequest,
+)
+request = json_format.Parse(json_spans, PB2ExportTraceServiceRequest())
+from google.protobuf import text_format
+print("protobuf(text):")
+text_format.PrintMessage(request, sys.stdout))
