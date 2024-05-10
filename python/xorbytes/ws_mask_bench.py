680e6de36983e949d5da2e7a95544ae16b9e3d9a
Inada Naoki <songofacandy@gmail.com>
2024-05-10 15:25:39 +0900

    add benchmark using bytes(map(operator.xor

--- python/xorbytes/ws_mask_bench.py
+++ python/xorbytes/ws_mask_bench.py
@@ -1,6 +1,12 @@
 import random
 from tornado.util import _websocket_mask_python
-from tornado.speedups import websocket_mask
+import itertools
+import operator
+
+try:
+    from tornado.speedups import websocket_mask
+except ImportError:
+    websocket_mask = False
 import pyperf
 
 
@@ -10,18 +16,28 @@ def websocket_mask_int(mask: bytes, data: bytes) -> bytes:
     mask *= (len(data) + 3) // 4
     del mask[len(data) :]
     assert len(mask) == len(data)
-    return (int.from_bytes(mask) ^ int.from_bytes(data)).to_bytes(len(data))
+    return (int.from_bytes(mask, "big") ^ int.from_bytes(data, "big")).to_bytes(
+        len(data), "big"
+    )
+
+
+def mask_itertools(mask: bytes, data: bytes) -> bytes:
+    return bytes(map(operator.xor, data, itertools.cycle(mask)))
 
 
 mask = random.randbytes(4)
 data = random.randbytes(100)
 
 for i in range(15):
-    assert _websocket_mask_python(mask, data) == websocket_mask(mask, data)
-    assert websocket_mask_int(mask, data) == websocket_mask(mask, data)
+    if websocket_mask:
+        assert _websocket_mask_python(mask, data) == websocket_mask(mask, data)
+    assert websocket_mask_int(mask, data) == _websocket_mask_python(mask, data)
+    assert mask_itertools(mask, data) == _websocket_mask_python(mask, data)
 
 
 runner = pyperf.Runner()
 runner.bench_func("_websocket_mask_python", _websocket_mask_python, mask, data)
-runner.bench_func("tornado.speedups", websocket_mask, mask, data)
+if websocket_mask:
+    runner.bench_func("tornado.speedups", websocket_mask, mask, data)
 runner.bench_func("websocket_mask_int", websocket_mask_int, mask, data)
+runner.bench_func("mask_itertools", mask_itertools, mask, data)
--- python/xorbytes/xor_bytes.py
+++ python/xorbytes/xor_bytes.py
@@ -1,5 +1,6 @@
 import pyperf
 import random
+import operator
 
 
 def xor_bytes_list(a, b):
@@ -10,22 +11,28 @@ def xor_bytes_generator(a, b):
     return bytes((aa ^ bb) for (aa, bb) in zip(a, b, strict=True))
 
 
+def xor_bytes_operator(a, b):
+    return bytes(map(operator.xor, a, b))
+
+
 def xor_bytes_via_int(a, b):
     if len(a) != len(b):
         raise ValueError(f"a and b must have same length; {len(a)=} {len(b)=}")
-    aa = int.from_bytes(a)
-    bb = int.from_bytes(b)
-    return (aa ^ bb).to_bytes(len(a))
+    aa = int.from_bytes(a, "big")
+    bb = int.from_bytes(b, "big")
+    return (aa ^ bb).to_bytes(len(a), "big")
 
 
 xa = bytes([*range(256)])
 xb = random.randbytes(256)
 
-assert (
-    xor_bytes_list(xa, xb) == xor_bytes_generator(xa, xb) == xor_bytes_via_int(xa, xb)
-)
+expected = xor_bytes_list(xa, xb)
+assert expected == xor_bytes_generator(xa, xb)
+assert expected == xor_bytes_operator(xa, xb)
+assert expected == xor_bytes_via_int(xa, xb)
 
 runner = pyperf.Runner()
 runner.bench_func("list", xor_bytes_list, xa, xb)
 runner.bench_func("generator", xor_bytes_generator, xa, xb)
+runner.bench_func("operator", xor_bytes_operator, xa, xb)
 runner.bench_func("int", xor_bytes_via_int, xa, xb)
